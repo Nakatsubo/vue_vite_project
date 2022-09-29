d866f57ebbe2dafeb18f440e5ecb303a11da5504
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-09-29 21:31:45 +0900

    パス設定の追加

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,26 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+
+  // Path
+  build: {
+    rollupOptions: { //ファイル出力設定
+      output: {
+        assetFileNames: (assetInfo) => {
+          let extType = assetInfo.name.split('.')[1];
+          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
+            extType = 'images';
+          }
+	  //ビルド時のCSS名を明記してコントロールする
+          if(extType === 'css') {
+            return `assets/css/style.css`;
+          }
+          return `assets/${extType}/[name][extname]`;
+        },
+        chunkFileNames: 'assets/js/[name].js',
+        entryFileNames: 'assets/js/[name].js',
+      },
+    },
+  }
 })
